50cad47c6a6af979e6520fcefc59fc4329f5a621
hamza iraqi <hz.iraqi@gmail.com>
2019-03-04 18:42:16 +0100

    homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
   has_many :transactions
   has_many :plants
   validates :name, presence: true, uniqueness: true
-  validates :name, presence: true, uniqueness: true
   validates :contact, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+User.destroy_all
+Plant.destroy_all
+
+simo = User.create(name: "simo", email: "mail@mail.com", password:"blabla", contact:"mail@mail.com")
+Plant.create(name: "Bergamotte", user:simo)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
