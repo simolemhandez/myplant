11fc48979cd21b5c64a2c7e554944edf0261a174
Simo Lemhandez <s.lemhandez@gmail.com>
2019-03-05 13:06:47 +0000

    new routes for plant

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -4,7 +4,7 @@ class PlantsController < ApplicationController
   end
 
   def show
-    @plant = plant.find(params[:id])
+    @plant = Plant.find(params[:id])
   end
 
   def new
@@ -13,13 +13,31 @@ class PlantsController < ApplicationController
 
   def create
     @plant = Plant.new(plant_params)
+    @plant.user = current_user
+    # raise
     @plant.save
-    redirect_to plant_path(plant)
+    redirect_to plants_path
+  end
+
+  def edit
+    @plant = Plant.find(params[:id])
+  end
+
+  def update
+    @plant = Plant.find(params[:id])
+    @plant.update(plant_params)
+    redirect_to plant_path(@plant)
+  end
+
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+    redirect_to plants_path
   end
 
   private
 
   def plant_params
-    params.require(:plant).permit(:id, :name, :location, :contact, :photo)
+    params.require(:plant).permit(:id, :name, :description, :origin, :availability, :pricepermonth, :photo, :photo_cache)
   end
 end
