5ced11e85076f8750ab6c93e86d70fdbff31626a
Simo Lemhandez <s.lemhandez@gmail.com>
2019-03-05 14:37:59 +0000

    changed new routes for plant and pundit

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,10 +1,11 @@
 class PlantsController < ApplicationController
   def index
-    @plants = Plant.all
+    @plants = policy_scope(Plant)
   end
 
   def show
     @plant = Plant.find(params[:id])
+    authorize @plant
   end
 
   def new
