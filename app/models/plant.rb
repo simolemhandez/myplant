3bac92cb2183ffb961485151eca1ec2b7cbf4732
Simo Lemhandez <s.lemhandez@gmail.com>
2019-03-05 11:10:17 +0000

    changed the model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,5 @@
 class Plant < ApplicationRecord
   has_many :transactions
+  belongs_to :user
   validates :name, presence: true, uniqueness: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,10 @@
 class Transaction < ApplicationRecord
   belongs_to :plant
+  belongs_to :user
   validates :plant_id, presence: true
   validates :plant_id, uniqueness: { scope: :plant_id }
-  validates :tenant_id, presence: true
-  validates :tenant_id, uniqueness: { scope: :user_id }
+  validates :user_id, presence: true
+  validates :user_id, uniqueness: { scope: :user_id }
   validates :time, presence: true, uniqueness: true
   validates :date, presence: true, uniqueness: true
 end
