e163a18d3818ffc2f98f1e3ef6b828ed991ee69e
hamza iraqi <hz.iraqi@gmail.com>
2019-03-05 15:41:56 +0100

    translation to french

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2019_03_05_113842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: <true></true>
   end
 
   add_foreign_key "plants", "users"
