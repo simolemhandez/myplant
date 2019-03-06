0b26371e357fc258ea3136a6b8dcfd589e97a5b8
hamza iraqi <hz.iraqi@gmail.com>
2019-03-06 14:34:42 +0100

    added mapbox api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   has_many :plants
   has_many :transactions
   mount_uploader :photo, PhotoUploader
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
