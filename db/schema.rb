1a52b2d8b8945c3eeaffb5386270abfa25f8785f
hamza iraqi <hz.iraqi@gmail.com>
2019-03-07 11:04:34 +0100

    added address to plants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_163357) do
+ActiveRecord::Schema.define(version: 2019_03_07_100333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 2019_03_05_163357) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_plants_on_user_id"
   end
 
@@ -51,6 +54,8 @@ ActiveRecord::Schema.define(version: 2019_03_05_163357) do
     t.string "name"
     t.string "location"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
